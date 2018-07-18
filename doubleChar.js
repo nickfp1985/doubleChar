eb4161aaf5f2dab9449269f6483d9514b281ac22
Nick Peddie <nfpeddie@gmail.com>
2018-07-18 10:08:41 -0700

    added js file
    added js file

---
+++ doubleChar.js
@@ -0,0 +1,9 @@
+function doubleChar(str) {
+
+  let repeatString = [];
+  for(let i = 0; i <= repeatString.length-1; i++){
+  repeatString.push(str[i]);
+  repeatString.push(str[i]);
+  }
+  return repeatString.join('');
+}
